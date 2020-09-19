d11e3271bc2410f792448e10a3209fd936b1e718
leiguoqing <leiguoqing520.00>
2020-09-19 23:31:03 +0800

    feature: 添加/修改任务时添加对执行类名、执行参数、表达式的验证

--- src/main/java/com/leigq/quartz/controller/SysTaskController.java
+++ src/main/java/com/leigq/quartz/controller/SysTaskController.java
@@ -2,12 +2,17 @@ package com.leigq.quartz.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.leigq.quartz.bean.common.Response;
+import com.leigq.quartz.bean.job.BaseTaskExecute;
 import com.leigq.quartz.bean.vo.AddSysTaskVO;
 import com.leigq.quartz.bean.vo.SysTaskListVO;
 import com.leigq.quartz.bean.vo.UpdateSysTaskVO;
 import com.leigq.quartz.service.SysTaskService;
+import com.leigq.quartz.util.SpringContextHolder;
+import com.leigq.quartz.util.ValidUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.quartz.CronExpression;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -45,10 +50,9 @@ public class SysTaskController {
 	 */
 	@PostMapping("/tasks")
 	public Response addTask(@Valid AddSysTaskVO addSysTaskVO) {
-		// 验证表达式格式
-		if (!CronExpression.isValidExpression(addSysTaskVO.getCron())) {
-			return Response.fail("表达式格式错误！");
-		}
+		this.checkTaskClassName(addSysTaskVO.getTaskClass());
+		this.checkExecParams(addSysTaskVO.getExecParams());
+		this.checkCron(addSysTaskVO.getCron());
 		sysTaskService.addTask(addSysTaskVO);
 		return Response.success("创建定时任务成功！");
 	}
@@ -67,10 +71,9 @@ public class SysTaskController {
 	 */
 	@PutMapping("/tasks")
 	public Response updateTask(@Valid UpdateSysTaskVO updateSysTaskVO) {
-		// 验证表达式格式
-		if (!CronExpression.isValidExpression(updateSysTaskVO.getCron())) {
-			return Response.fail("表达式格式错误！");
-		}
+		this.checkTaskClassName(updateSysTaskVO.getTaskClass());
+		this.checkExecParams(updateSysTaskVO.getExecParams());
+		this.checkCron(updateSysTaskVO.getCron());
 		sysTaskService.updateTask(updateSysTaskVO);
 		return Response.success("更新任务成功！");
 	}
@@ -160,5 +163,50 @@ public class SysTaskController {
 	public Response queryTaskList(@PathVariable("page_num") Integer pageNum, @PathVariable("page_size") Integer pageSize) {
 		final IPage<SysTaskListVO> sysTaskList = sysTaskService.taskList(pageNum, pageSize);
 		return Response.success(sysTaskList);
+	}
+
+
+	/**
+	 * 验证执行参数格式
+	 * @param execParams 执行参数
+	 */
+	private void checkExecParams(String execParams) {
+		if (StringUtils.isNotBlank(execParams)) {
+			ValidUtils.checkArg(!execParams.contains("="), "执行参数格式错误");
+		}
+	}
+
+
+	/**
+	 * 验证表达式格式
+	 * @param cron 表达式
+	 */
+	private void checkCron(String cron) {
+		// 验证表达式格式
+		if (!CronExpression.isValidExpression(cron)) {
+			ValidUtils.throwException("表达式格式错误！");
+		}
+	}
+
+	/**
+	 * 检验任务执行类名时候正确
+	 * @param taskClass 执行类名
+	 */
+	private void checkTaskClassName(String taskClass) {
+		try {
+			// 获取自定义任务的类
+			Class<?> clazz = Class.forName(taskClass);
+			if (BaseTaskExecute.class.getSimpleName().equals(clazz.getSimpleName())) {
+				ValidUtils.throwException(String.format("执行类不可配置为%s！", BaseTaskExecute.class.getSimpleName()));
+			}
+			// 获取自定义任务实例，自定义任务全部继承 BaseTaskExecute
+			BaseTaskExecute baseTaskExecute = (BaseTaskExecute) SpringContextHolder.getBean(clazz);
+		} catch (ClassNotFoundException e) {
+			ValidUtils.throwException("找不到执行类，请检查执行类是否配置正确！");
+		} catch (NoSuchBeanDefinitionException e) {
+			ValidUtils.throwException("找不到执行类，请检查执行类是否配置@Component注解！");
+		} catch (ClassCastException e) {
+			ValidUtils.throwException(String.format("请确保执行类继承%s类！", BaseTaskExecute.class.getSimpleName()));
+		}
 	}
 }
