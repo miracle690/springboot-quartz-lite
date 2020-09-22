e0b89214d90d7bb2207e65ee88a543a66b054108
leigq <191789784@qq.com>
2020-09-22 10:21:24 +0800

    feature: 添加正在执行状态

--- src/main/resources/sql/自定义任务和任务日志表.sql
+++ src/main/resources/sql/自定义任务和任务日志表.sql
@@ -12,7 +12,7 @@ CREATE TABLE `sys_task`
     `cron`        varchar(50) DEFAULT NULL COMMENT '定时规则',
     `exec_params` text COMMENT '执行参数',
     `exec_date`   datetime    DEFAULT NULL COMMENT '执行时间',
-    `exec_result` tinyint(1)  DEFAULT NULL COMMENT '执行结果（成功:1、失败:0)',
+    `exec_result` tinyint(1)  DEFAULT NULL COMMENT '执行结果（成功:1、失败:0、正在执行：-1)',
     `concurrent`  tinyint(1)  DEFAULT '0' COMMENT '是否允许并发，0(false)：不允许 1（true）：允许',
     `create_time` datetime    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `creator`     bigint(20)  DEFAULT NULL COMMENT '创建人',
@@ -38,7 +38,7 @@ CREATE TABLE `sys_task_log`
     `id`               bigint(64) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
     `task_name`        varchar(50) DEFAULT NULL COMMENT '任务名',
     `exec_date`        datetime    DEFAULT NULL COMMENT '执行时间',
-    `exec_result`      tinyint(1)  DEFAULT NULL COMMENT '执行结果（成功:1、失败:0)',
+    `exec_result`      tinyint(1)  DEFAULT NULL COMMENT '执行结果（成功:1、失败:0、正在执行：-1)',
     `exec_result_text` text COMMENT '成功信息或抛出的异常信息',
     `task_id`          bigint(20)  DEFAULT NULL COMMENT '任务id',
     PRIMARY KEY (`id`)
